b33d40b6877fce6dd49aa17b4d6337c3d81fe599
vmware <932478753@qq.com>
2023-10-12 23:15:52 +0800

    RBTREE

--- C++/RBTree/RBTree.cpp
+++ C++/RBTree/RBTree.cpp
@@ -45,10 +45,6 @@ public:
     {
         return RotateL(t);
     }
-    void RotateR(RBNode<Type>* t)
-    {
-        return RotateR(t);
-    }
     void RotateLR(RBNode<Type>* t)
     {
         return RotateLR(t);
@@ -60,17 +56,37 @@ public:
 protected:
     void Insert(RBNode<Type>*& t, const Type& v);
     void Insert_FIXUP(RBNode<Type>*& t, RBNode <Type>* x);
+    void RotateR(RBNode<Type>* t, RBNode<Type>* ptr);
 protected:
     RBNode<Type>* BuyNode(const Type& v = Type())
     {
         RBNode<Type>* s = new RBNode<Type>(v);
         s->leftChild = s->rightChild = s->parent = NIL;
         return s;
-    }
+    }    
 private:
     RBNode<Type>* NIL;//外部节点，为黑色
     RBNode<Type>* root;
 };
+
+template<class Type>   
+void RBTree<Type>::RotateR(RBNode<Type>* t, RBNode<Type>* ptr)
+{
+    RBNode<Type>* R = ptr;
+    ptr = R->leftChild;
+    R->leftChild = ptr->rightChild;
+    if (ptr->rightChild != NIL)
+        ptr->rightChild->parent = R;
+    if (R == t)
+        t = ptr;
+    else if (R == R->parent->leftChild)
+        R->parent->leftChild = ptr;
+    else
+        R->parent->rightChild = ptr;
+    ptr->rightChild = R;
+    R->parent = ptr;
+
+}
 
 template <class Type>
 void RBTree<Type>::Insert(RBNode<Type>*& t, const Type& v)
@@ -88,7 +104,7 @@ void RBTree<Type>::Insert(RBNode<Type>*& t, const Type& v)
             x = x->rightChild;
     }
     x = BuyNode(v);
-    if (pr == NIL)
+    if (pr == NIL)  //为空树的时候插入
         t = x;
     else if (x->data < pr->data)
         pr->leftChild = x;
@@ -104,18 +120,39 @@ void RBTree<Type>::Insert_FIXUP(RBNode<Type>*& t, RBNode<Type>* x)
 {
     while (x != t && x->parent->color == RED)//若父结点颜色为黑直接插入，可以推出来，父结点为黑的时候插入不影响平衡，因为父结点为黑时，高度不是最大的，插入一个红色的结点并不会超过最大高度
     {
-        RBNode<Type>* s = x->parent->parent->rightChild;
+        //父结点为祖父结点的左子树
         if (x->parent->parent->leftChild == x->parent)
         {
-            //叔伯结点为红色
-            if (s->color = red)
+            RBNode<Type>* s = x->parent->parent->rightChild;
+            //叔伯结点为红色 
+            if (s->color == RED)
             {
-                
+                //情况3，叔伯结点为红，不旋转，直接更改颜色
+                x->parent->color = BLACK;
+                s->color = BLACK;
+                x->parent->parent->color = RED;
+                //检查祖父的父结点的颜色 GG->color
+                x = x->parent->parent;
             }
             else
             {
+                //叔伯结点为黑色，情况分外侧插入和内侧插入
+                if (x->parent->rightChild == x)//内侧 <
+                {
+                    //情况2，双旋转
 
+
+                }
+                //内侧
+                //情况1,单旋转,更改P、G颜色   /
+                x->parent->color = BLACK;
+                x->parent->parent->color = RED;
+                RotateR(t, x->parent->parent);
             }
+        }
+        else
+        {
+
         }
     }
     t->color = BLACK;
@@ -124,11 +161,13 @@ void RBTree<Type>::Insert_FIXUP(RBNode<Type>*& t, RBNode<Type>* x)
 int main()
 {
     RBTree<int> rb;
-    int ar[] = { 8,9,7,6,4,3,10 };
+    int ar[] = { 8,3,10,7 };
     int n = sizeof(ar) / sizeof(ar[0]);
     for (int i = 0; i < n; i++)
     {
         rb.Insert(ar[i]);
     }
+    rb.Insert(6);
+
     return 0;
 }
