eff97e57938d1c58318938dc05f476db5c6e153a
vmware <932478753@qq.com>
2023-10-31 21:56:44 +0800

    chatsystem

---
+++ Linux/ChatSystem/src/main.cpp
@@ -0,0 +1,37 @@
+#include "DataBaseServer.hpp"
+int main()
+{
+  DataBaseServer dbs;
+  if(!dbs.MySqlInit())
+  {
+    cout << "MysqlInit failed" << endl;
+    return 0;
+  }
+  vector<int> f_id;
+  if(!dbs.GetFriend(2, &f_id))
+  {
+    cout << "GetFriend failed" << endl;
+    return 0;
+  }
+  for(size_t i = 0; i <f_id.size(); i++)
+  {
+    cout << f_id[i] << " ";
+  }
+  cout << endl;
+  /* 
+   *插入用户信息
+  if(!dbs.InsertUser(5, "djt", "xaufe", "13538829343", "djt3422"))
+  {
+    cout << "Insert User falied" << endl;
+    return 0;
+  }
+  */
+  /*添加好友
+  if(!dbs.InsertFriend(5, 1))
+  {
+    cout << "Insert friend failed" << endl;
+  }
+  */
+  dbs.GetAllUser();
+  return 0;
+}
