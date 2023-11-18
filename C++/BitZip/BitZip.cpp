051ef4bedce55470ac4979be3df39fa829e75989
Nalizhi <932478753@qq.com>
2023-11-18 23:04:42 +0800

    gitzip

---
+++ C++/BitZip/BitZip.cpp
@@ -0,0 +1,15 @@
+﻿// BitZip.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//Huffman编码压缩的过程
+/*	1.统计源文件每个字节出现的个数
+*	2.用字节频数创建Huffman树
+*	3.获取每个字节的Huffman编码
+*	4.用得到的编码对源文件中的每个字节进行改写
+*/
+#include "HuffmanTree.hpp"
+
+int main()
+{
+	std::vector<int> vt = { 3,1,5,7 };
+	HuffmanTree<int>h(vt);
+}
+
---
+++ C++/BitZip/HuffmanTree.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <vector>
+#include <queue>
+template<class _Ty>
+struct HuffmanTreeNode
+{
+	HuffmanTreeNode<_Ty>* _left;
+	HuffmanTreeNode<_Ty>* _right;
+	_Ty _weight;
+	HuffmanTreeNode(const _Ty& weight = _Ty())
+		: _left(nullptr)
+		, _right(nullptr)
+		, _weight(weight)
+	{}
+};
+
+template <class _Ty>
+class HuffmanTree
+{
+	typedef HuffmanTreeNode<_Ty> Node;
+	class Compare
+	{
+	public:
+		bool operator()(const Node* left, const Node* right)
+		{
+			return left->_weight > right->_weight;
+		}
+	};
+public:
+	HuffmanTree()
+		:_root(nullptr)
+	{}
+
+	HuffmanTree(const std::vector<_Ty>& vw)
+	{
+		//1.用所有的权值构造只有根节点的二叉树森林
+		//森林中二叉树应该使用堆（优先级队列）来保存
+		std::priority_queue< Node*, std::vector<Node*>, Compare> q;
+		for (auto& e : vw)
+		{
+			q.push(new Node(e));
+		}
+		while (q.size() > 1)
+		{
+			Node* left = q.top();
+			q.pop();
+
+			Node* right = q.top();
+			q.pop();
+
+			Node* parent = new Node(left->_weight + right->_weight);
+			parent->_left = left;
+			parent->_right = right;
+
+			q.push(parent);
+		}
+		_root = q.top();
+	}
+private:
+	Node* _root;
+};
