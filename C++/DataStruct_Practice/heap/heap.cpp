93ce6fd763a58fa36ebdb2090073e11d1c75fc8c
vmware <932478753@qq.com>
2023-12-18 21:57:04 +0800

    heap

--- C++/DataStruct_Practice/heap/heap.cpp
+++ C++/DataStruct_Practice/heap/heap.cpp
@@ -1,34 +1,75 @@
 #include <iostream>
-#include <vector>
+#include <assert.h>
+#define HeapElemType int
 using namespace std;
 
-class Heap
+typedef struct Heap
 {
-public:
-  Heap():_arr(nullptr), _capacity(0), _size(0)
-  {}
-  Heap(size_t capacity):_capacity(capacity), _size(0)
-  {
-    _arr = new int(capacity); 
-  }
-  ~Heap()
-  {
-    delete _arr;
-  }
-public:
-  bool InsertHeap(int data)
-  {
-    
-    return true;
-  }
-private:
-  int* _arr;
-  size_t _capacity;
-  size_t _size;
-};
+  HeapElemType* hp;
+  size_t capacity;
+  size_t size;
+}Heap;
 
+void HeapInit(Heap* php, int sz);
+void HeapInsert(Heap* php, HeapElemType v);
+bool HeapFull(Heap* php)
+{
+  return php->size >= php->capacity;
+}
+void AdjustUp(Heap* php, int start);
+void HeapInit(Heap* php, int sz)
+{
+  php->capacity = sz;
+  php->hp = (HeapElemType*)malloc(sizeof(HeapElemType)*php->capacity);
+  assert(php->hp!=nullptr);
+  php->size = 0;
+}
+void HeapInsert(Heap* php, HeapElemType v)
+{
+  if(HeapFull(php))
+  {
+    cout << "堆空间已满，不能插入" << endl;
+  }
+  php->hp[php->size] = v;
+  AdjustUp(php, php->size);
+  php->size++;
+}
+void AdjustUp(Heap* php, int start)
+{
+  int j = start;
+  int i = (j - 1) / 2;
+  while(j > 0)
+  {
+    if(php->hp[j] < php->hp[i])
+    {
+      HeapElemType tmp = php->hp[j];
+      php->hp[j] = php->hp[i];
+      php->hp[i] = tmp;
+      
+      j = i;
+      i = (j-1) / 2;
+    }
+    else 
+      break;
+  }
+}
+void HeapShow(Heap* php)
+{
+  for(size_t i = 0; i < php->size; i++)
+  {
+    cout << php->hp[i] << endl;
+  }
+}
 int main()
 {
-  vector<int> vt = {3, 6, 7, 4, 9, 14, 2};
+  int ar[] = {27,15,19,18,28,34,65,49,25,37};
+  int n = sizeof(ar) / sizeof(ar[0]);
+  Heap hp;
+  HeapInit(&hp, 16);
+  for(int i = 0; i < n; i++)
+  {
+    HeapInsert(&hp, ar[i]);
+  }
+  HeapShow(&hp);
   return 0;
 }
